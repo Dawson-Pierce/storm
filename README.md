2f8680ad64fdd9ea58d88a8b49929d5000341759
Dawson Pierce <123189933+Dawson-Pierce@users.noreply.github.com>
2026-09-10 17:03:56 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+### STORM Tracking Algorithms  
---
+++ cpp/include/storm/types.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <Eigen/Dense>
+
+namespace storm {
+
+    using Vector = Eigen::VectorXd;
+    using Matrix = Eigen::MatrixXd;
+}
